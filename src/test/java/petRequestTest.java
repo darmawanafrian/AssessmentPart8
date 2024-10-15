da7399c654845632400becc92dc79745c8df26b2
afriandarmawan <darmawanafrian@gmail.com>
2024-10-15 10:04:44 +0700

    first commit

--- src/test/java/petRequestTest.java
+++ src/test/java/petRequestTest.java
@@ -1,2 +1,103 @@
-package PACKAGE_NAME;public class petRequestTest {
+
+import io.qameta.allure.Feature;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+@Feature("Pet Module")
+public class petRequestTest extends BaseTest {
+    @Test(priority = 1)
+    public void addPetTest(){
+        String requestBody = "{\n" +
+                "  \"id\": 1111,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1111,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"buldog\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1111,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .log().ifValidationFails()
+                .when()
+                .post()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test(priority = 2)
+    public void findPetByIdTest(){
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/1111")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    @Test(priority = 3)
+    public void updatePetTest(){
+        String requestBody = "{\n" +
+                "  \"id\": 1111,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1111,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"buldog\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1111,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"sold\"\n" +
+                "}";
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .log().ifValidationFails()
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+    }
+    @Test(priority = 4)
+    public void findPetByStatusTest(){
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/findByStatus?status=sold")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+    @Test(priority = 5)
+    public void deletePetTest(){
+        given().header("api_key","some_key")
+                .when()
+                .get("/1111")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
 }
+
--- src/test/java/userRequestTest.java
+++ src/test/java/userRequestTest.java
@@ -1,2 +1,97 @@
-package PACKAGE_NAME;public class userRequestTest {
+import io.qameta.allure.Feature;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+@Feature("user Module")
+public class userRequestTest extends BaseUserTest{
+    @Test(priority = 1)
+    public void createUserTest(){
+        String requestBody = "{\n" +
+                "  \"id\": 111,\n" +
+                "  \"username\": \"afriandarmawan\",\n" +
+                "  \"firstName\": \"afrian\",\n" +
+                "  \"lastName\": \"darmawan\",\n" +
+                "  \"email\": \"aa@gmail.com\",\n" +
+                "  \"password\": \"123456789\",\n" +
+                "  \"phone\": \"08111111111\",\n" +
+                "  \"userStatus\": 1\n" +
+                "}";
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .log().ifValidationFails()
+                .when()
+                .post()
+                .then()
+                .statusCode(200);
+    }
+
+@Test(priority = 2)
+    public void userLoginTest(){
+        given().queryParam("afriandarmawan")
+                .queryParam("123456789")
+                .when()
+                .get("/login")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    @Test(priority = 3)
+    public void findUserByNameTest(){
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .get("/afriandarmawan")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    @Test(priority = 4)
+    public void updateUserTest(){
+        String requestBody = "{\n" +
+                "  \"id\": 111,\n" +
+                "  \"username\": \"afriandarmawan\",\n" +
+                "  \"firstName\": \"afrian\",\n" +
+                "  \"lastName\": \"darmawan\",\n" +
+                "  \"email\": \"bb@gmail.com\",\n" +
+                "  \"password\": \"123456789\",\n" +
+                "  \"phone\": \"08111111111\",\n" +
+                "  \"userStatus\": 0\n" +
+                "}";
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(requestBody)
+                .log().ifValidationFails()
+                .when()
+                .put("/afriandarmawan")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    @Test(priority = 5)
+    public void userLogoutTest(){
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/logout")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+    @Test(priority = 6)
+    public void deleteUserTest(){
+        given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/afriandarmawan")
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
 }
